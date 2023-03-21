bf94c44787f07dc1a27c4d21ac3d13500c3a84b6
wagner <wagner.santos@accurate.com.br>
2023-03-21 16:29:19 -0300

    Criado e implementado metodo saca

--- aula_001_classe/src/Conta.java
+++ aula_001_classe/src/Conta.java
@@ -4,7 +4,15 @@ public class Conta {
     int numero;
     String titular;
 
-    void deposita(double valor){
+    public void deposita(double valor){
         this.saldo += valor;
+    }
+
+    public boolean saca(double valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
     }
 }
--- aula_001_classe/src/TestaMetodos.java
+++ aula_001_classe/src/TestaMetodos.java
@@ -3,7 +3,19 @@ public class TestaMetodos {
         Conta conta = new Conta();
         conta.saldo = 190;
         System.out.println(conta.saldo);
+
         conta.deposita(810);
         System.out.println(conta.saldo);
+
+        boolean saquePrimeiraTentativa = conta.saca(10);
+        System.out.println(saquePrimeiraTentativa);
+
+        System.out.println(conta.saldo);
+
+        boolean saqueSegundaTentativa = conta.saca(1000);
+        System.out.println(saqueSegundaTentativa);
+
+        System.out.println(conta.saldo);
+
     }
 }
