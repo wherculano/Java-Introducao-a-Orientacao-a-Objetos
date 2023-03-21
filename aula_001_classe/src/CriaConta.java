d123ec5f63a14e05c6c3872e958967dceae4710b
wagner <wagner.santos@accurate.com.br>
2023-03-21 15:21:07 -0300

    Criada segunda instancia

--- aula_001_classe/src/CriaConta.java
+++ aula_001_classe/src/CriaConta.java
@@ -3,5 +3,11 @@ public class CriaConta {
         Conta primeiraConta = new Conta();
         primeiraConta.saldo = 200.00;
         System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 50;
+
+        System.out.println("A primeira conta tem R$" + primeiraConta.saldo);
+        System.out.println("A segunda conta tem R$" +segundaConta.saldo);
     }
 }
