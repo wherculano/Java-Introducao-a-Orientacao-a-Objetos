e8aa88ba08893a1a4dc9140c0248fd1b01893802
wagner <wagner.santos@accurate.com.br>
2023-03-22 08:41:43 -0300

    Exemplo de composicao de objetos

---
+++ aula_002_objeto_composto/src/TestaBanco.java
@@ -0,0 +1,16 @@
+public class TestaBanco {
+    public static void main(String[] args){
+        Cliente wagner = new Cliente();
+        wagner.nome = "Wagner Herculano";
+        wagner.cpf = "999.999.999-99";
+        wagner.profissao = "Programador";
+
+        Conta contaDoWagner = new Conta();
+        contaDoWagner.deposita(1000);
+        contaDoWagner.titular = wagner;
+
+        System.out.println(contaDoWagner.titular.nome);
+        System.out.println(contaDoWagner.saldo);
+
+    }
+}
